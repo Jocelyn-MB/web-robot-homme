d2595bd017a878e5f5e1efbda66fa7130b4ee9a3
Jocelyn-MB <mancillablancasjocelyn@gmail.com>
2024-03-25 01:11:03 -0600

    first commit

---
+++ app/components/utils/specifications.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+
+export default function QuoteSection() {
+  return (
+    <section className="quote__section w-full relative bg-white mx-auto px-8 lg:w-4/5"> {/* Estableciendo un ancho específico */}
+      <div className="grid grid-cols-4 gap-sm mx-xxs md:grid-cols-12 md:gap-lg md:mx-sm py-lg quote__section__grid grid-flow-dense lg:h-full">
+        <figure className="relative overflow-hidden aspect-portrait col-span-full md:col-span-6">
+          <Image
+            src="/images/robotsito.jpg"
+            alt="Kasim Kutay"
+            width={3840}
+            height={2560}
+            layout="responsive"
+          />
+        </figure>
+        <div className="flex flex-col justify-center col-span-full mt-12 md:mt-0 text-dark h-full md:col-span-6 lg:p-8"> {/* Aplicando padding al texto */}
+          <div className="max-w-lg"> {/* Estableciendo un ancho máximo para el contenido de texto */}
+            <blockquote className="mb-11">
+              <div className="font-novoApply styles_richtext__vtvls styles_quote__block__aHUoR">
+                <h2>Reconocemos que la salud de las personas y la salud de nuestro planeta están entrelazadas y estamos decididos a desempeñar nuestro papel para asegurar un futuro más sostenible.</h2>
+              </div>
+            </blockquote>
+            <p className="text-dark font-novoApply text-body">Kasim Kutay</p>
+            <p className="text-dark font-novoApply text-body">Director ejecutivo, Novo Holdings</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
