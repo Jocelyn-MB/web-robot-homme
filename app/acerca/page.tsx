012edf73becac42b9abaab7d2f1f5b1b760fb1cc
Jocelyn-MB <mancillablancasjocelyn@gmail.com>
2024-04-11 23:49:11 -0600

    acerca completado

--- app/acerca/page.tsx
+++ app/acerca/page.tsx
@@ -7,46 +7,112 @@ import AcercaDe from '@/public/images/AcercaDe.jpg';
 export default function ContactForm() {
     return (
        <section className="relative">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className=" px-0 sm:px-0">
 
-        {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
-          {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-3xl md:text-4xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">ROBOT AUTOMATIZADO PARA CUIDADO Y MONITOREO DE ADULTOS MAYORES </h1>
-         </div>
+          {/* Hero content */}
+          <div className="pt-32 pb-12 md:pt-20 md:pb-10">
   
-          {/* Section Poster*/}
-         <div className="grid grid-cols-4 gap-sm mx-xxs md:grid-cols-12 md:gap-lg md:mx-sm py-lg quote__section__grid grid-flow-dense lg:h-full pt-6 pb-6">
-            <figure className="relative overflow-hidden aspect-portrait col-span-full md:col-span-6">
-                <Image
-                  src="/images/homeeAcerdadejpg-gigapixel-standard.png"
-                  alt="Proyecto Homme"
-                  width={3840}
-                  height={2560}
-                  layout="responsive"
-                />
-              </figure>
-              <div className="relative flex flex-col justify-center p-6 col-span-full mt-12 md:mt-0 text-dark h-full md:col-span-6 lg:p-8"> {/* Aplicando padding al texto */}
-                <div className="max-w-lg"> 
-                  <blockquote className="mb-11">
-                    <div className="font-novoApply styles_richtext__vtvls styles_quote__block__aHUoR">
-                    <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 pb-4">Acerca del Proyecto</h4>
-                    <h2>El proyecto empleó la metodología ágil Scrum, comenzando con una planificación inicial para definir el backlog del producto y establecer hitos. Se formó un equipo multidisciplinario y se llevó a cabo un Sprint 0 para investigar y preparar el desarrollo. El trabajo se dividió en iteraciones de dos semanas, cada una concluyendo con revisiones y retrospectivas.</h2>
+            {/* Section Introduccion*/}
+            <div className="grid grid-cols-4 gap-sm mx-xxs md:grid-cols-12 md:gap-lg md:mx-sm py-lg quote__section__grid grid-flow-dense lg:h-full  pb-14">
+              {/* Imagen */}
+                <figure className="relative overflow-hidden aspect-portrait col-span-full md:col-span-6">
+                  <Image
+                    src="/images/homeeAcerdadejpg-gigapixel-standard.png"
+                    alt="Proyecto Homme"
+                    width={3840}
+                    height={2560}
+                    layout="responsive"
+                  />
+                </figure>
+                  {/* Texto */}
+                  <div className="relative flex flex-col justify-center p-6 col-span-full md:mt-0 md:col-span-6 lg:p-8"> {/* Aplicando padding al texto */}
+                    <div className="max-w-lg">
+                      {/* Título */}
+                      <div className="text-center md:text-left mb-6">
+                        <h1 className="text-3xl md:text-4xl font-extrabold leading-tighter tracking-tighter mb-4 text-right bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400" data-aos="zoom-y-out">ROBOT AUTOMATIZADO PARA CUIDADO Y MONITOREO DE ADULTOS MAYORES</h1>
+                      </div>
+                      {/* Texto */}
+                      <blockquote className="mb-11">
+                        <div className="font-novoApply styles_richtext__vtvls styles_quote__block__aHUoR text-right">
+                          <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 pb-4">Integradora II</h4>
+                          <h2>Tecnologías de la Inforcación Área Desarrollo de Softaware Multiplataforma</h2>
+                        </div>
+                      </blockquote>
                     </div>
-                  </blockquote>
-                  <p className="text-dark font-novoApply text-body"></p>
-                  <p className="text-dark font-novoApply text-body"></p>
+                  </div>
                 </div>
-            </div>
-          </div>
-          {/* Section Poster*/}
-          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            <Image src={AcercaDe} width={708} height={772} layout="responsive" alt="Illustration" />
-          </div>
+                
+                {/* Section Desarrollo*/}
+                <div className="grid grid-cols-4 gap-sm mx-xxs md:grid-cols-12 md:gap-lg md:mx-sm py-lg quote__section__grid grid-flow-dense lg:h-full pt-14 pb-14">
+                {/* Problematica */}
+                <div className="relative flex flex-col justify-center p-6 col-span-full md:mt-0 md:col-span-4 lg:p-8"> {/* Aplicando padding al texto */}
+                      <div className="max-w-lg">
+                        {/* Título */}
+                        <div className="text-center md:text-left mb-6">
+                          <h1 className="text-2xl md:text-3xl font-extrabold leading-tighter tracking-tighter mb-4 text-right bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400" data-aos="zoom-y-out">Problematica</h1>
+                        </div>
+                        {/* Texto */}
+                        <blockquote className="mb-11">
+                          <div className="font-novoApply styles_richtext__vtvls styles_quote__block__aHUoR text-right pl-11">
+                            <h2>La distancia y la falta de supervisión hacen difícil cuidar a los adultos mayores. La soledad afecta su salud mental. La propuesta de un Robot Automatizado busca solucionar esto con tecnología IoT y sensores, mejorando su calidad de vida y tranquilizando a familiares y cuidadores.</h2>
+                          </div>
+                        </blockquote>
+                      </div>
+                    </div>
+                  {/* Imagen */}
+                  <figure className="relative overflow-hidden aspect-portrait col-span-full md:col-span-4 pr-6 pl-6">
+                    <Image
+                      src="/images/hommeAlcance.jpg"
+                      alt="Proyecto Homme"
+                      width={3840}
+                      height={2560}
+                      layout="responsive"
+                    />
+                  </figure>
+                    {/* Alcance */}
+                    <div className="relative flex flex-col justify-center p-6 col-span-full md:mt-0 md:col-span-4 lg:p-8"> {/* Aplicando padding al texto */}
+                      <div className="max-w-lg">
+                        {/* Título */}
+                        <div className="text-center md:text-left mb-6 ">
+                          <h1 className="text-2xl md:text-3xl font-extrabold leading-tighter tracking-tighter mb-4 text-left bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400" data-aos="zoom-y-out">Alcance</h1>
+                        </div>
+                        {/* Texto */}
+                        <blockquote className="mb-11">
+                          <div className="font-novoApply styles_richtext__vtvls styles_quote__block__aHUoR text-left pr-11">
+                            <h2>El proyecto consiste en crear un Robot Automatizado para el cuidado de adultos mayores en San Juan del Río. Esto implica diseñar, construir e integrar sensores y tecnología IoT en el robot, así como programar sistemas en ESP32. También se establecerá una conexión con una plataforma en la nube para el monitoreo remoto del robot</h2>
+                          </div>
+                        </blockquote>
+                      </div>
+                  </div>     
+                </div>
+                
+            {/* Section Vinil */}
+            <div className="grid md:grid-cols-12 max-w-6xl mx-auto px-4 sm:px-6 pt-14 pb-14 text-center">
+  {/* Imagen */}
+  <figure className="relative overflow-hidden aspect-portrait col-span-full md:col-start-3 md:col-span-8 text-center ">
+    <Image
+      src="/images/Vinil .jpg"
+      alt="Proyecto Homme"
+      width={3840}
+      height={2560}
+      layout="responsive"
+    />
+  </figure>
+</div>
+
+             {/* Video */}
+             <div className="grid md:grid-cols-12 max-w-6xl mx-auto px-4 sm:px-6 pb-14 text-center">
+                    {/* Aplicando padding al texto */}
+                    <div className="relative overflow-hidden aspect-portrait col-span-full md:col-start-3 md:col-span-8 text-center">
+                      {/* Video */}
+                      <video width="100%" controls>
+                        <source src="/videos/VideoHomme.mp4" type="video/mp4" />
+                        Tu navegador no soporta el elemento de video.
+                      </video>
+                    </div>
+                  </div>
         </div>
       </div>
     </section>
-    );
+  );
 }
--- app/components/utils/specifications.tsx
+++ app/components/utils/specifications.tsx
@@ -3,32 +3,33 @@ import Image from 'next/image';
 export default function QuoteSection() {
   return (
     <section className="quote__section w-full relative bg-white mx-auto px-8 lg:w-4/5">
-      <div className="grid grid-cols-1 md:grid-cols-12 md:gap-lg py-lg quote__section__grid lg:h-full">
-        <figure className="relative overflow-hidden aspect-portrait col-span-full md:col-span-6">
-          <Image
-            src="/images/robotsito.jpg"
-            alt="Kasim Kutay"
-            width={3840}
-            height={2560}
-            layout="responsive"
-          />
-        </figure>
-        <div className="flex flex-col justify-center col-span-full md:col-span-6 lg:p-8">
-          <div className="max-w-lg">
-            <blockquote className="mb-11">
-              <div className="font-novoApply styles_richtext__vtvls styles_quote__block__aHUoR">
-                <h3>Este proyecto desarrolla un Robot Automatizado con tecnología IoT y sensores para abordar los desafíos del envejecimiento de la población. Ofrece monitoreo continuo de las condiciones ambientales y la frecuencia cardíaca de adultos mayores en sus hogares, con alertas ante posibles emergencias.</h3>
-              </div>
-            </blockquote>
-            <p className="text-dark font-novoApply text-sm">Equipo de trabajo:</p>
-            <p className="text-dark font-novoApply text-sm">Carapia Hernandez Janet Leilani,</p>
-              <p className="text-dark font-novoApply text-sm">Mancilla Blancas Jocelyn Magali, </p>
-              <p className="text-dark font-novoApply text-sm">García Mendoza Ángel, 
-              </p>
-              <p className="text-dark font-novoApply text-sm">Becerril Rosillo Emilio </p>
-          </div>
+    <div className="grid grid-cols-1 md:grid-cols-12 md:gap-lg py-lg quote__section__grid lg:h-full">
+      <figure className="relative overflow-hidden aspect-portrait col-span-full md:col-span-6">
+        <Image
+          src="/images/robotsito.jpg"
+          alt="Kasim Kutay"
+          width={3840}
+          height={2560}
+          layout="responsive"
+          className="w-full h-auto"
+        />
+      </figure>
+      <div className="flex flex-col justify-center col-span-full md:col-span-6 md:px-8 md:py-8 lg:py-0 md:items-center md:justify-center">
+        <div className="max-w-lg">
+          <blockquote className="mb-11">
+            <div className="font-novoApply styles_richtext__vtvls styles_quote__block__aHUoR">
+              <h3>Este proyecto desarrolla un Robot Automatizado con tecnología IoT y sensores para abordar los desafíos del envejecimiento de la población. Ofrece monitoreo continuo de las condiciones ambientales y la frecuencia cardíaca de adultos mayores en sus hogares, con alertas ante posibles emergencias.</h3>
+            </div>
+          </blockquote>
+          <p className="text-dark font-novoApply text-sm">Equipo de trabajo:</p>
+          <p className="text-dark font-novoApply text-sm">Carapia Hernandez Janet Leilani,</p>
+          <p className="text-dark font-novoApply text-sm">Mancilla Blancas Jocelyn Magali, </p>
+          <p className="text-dark font-novoApply text-sm">García Mendoza Ángel, </p>
+          <p className="text-dark font-novoApply text-sm">Becerril Rosillo Emilio </p>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
+  
   );
 }
